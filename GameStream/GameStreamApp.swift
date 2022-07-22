e7d1554e0cd012da76aa2edc0c51c6b94e804fa3
joseDev <jose96corrza@gmail.com>
2022-07-22 17:41:08 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Jose Daniel Corredor Zambrano on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
